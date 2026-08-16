aee8ac0465f24710b66f957cb99c973850ad9c6b
surya-cogman <surya.roboengr@gmail.com>
2026-08-16 20:41:46 +0530

    cpp api

--- src/fr3_moveit_api_cpp/src/test_moveit.cpp
+++ src/fr3_moveit_api_cpp/src/test_moveit.cpp
@@ -1,14 +1,36 @@
 #include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
 
-int main(int argc,char **argv){
+int main(int argc, char **argv)
+{
 
-    rclcpp::init(argc,argv);
+    rclcpp::init(argc, argv);
 
     auto node = std::make_shared<rclcpp::Node>("test_moveit");
     rclcpp::executors::SingleThreadedExecutor executor;
     executor.add_node(node);
 
+    auto spinner = std::thread([&executor]()
+                               { executor.spin(); });
 
+    // move it groups of arm and gripper
+
+    auto arm_links = moveit::planning_interface::MoveGroupInterface(node, "arm_links");
+    arm_links.setMaxVelocityScalingFactor(0.5);
+    arm_links.setMaxAccelerationScalingFactor(0.5);
+
+    auto gripper_links = moveit::planning_interface::MoveGroupInterface(node, "gripper_links");
+
+    arm_links.setStartStateToCurrentState();
+    arm_links.setNamedTarget("Pose_1");
+
+    moveit::planning_interface::MoveGroupInterface::Plan plan1;
+    bool success = (arm_links.plan(plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success)
+    {
+        arm_links.execute(plan1);
+    }
 
     rclcpp::shutdown();
     return 0;
